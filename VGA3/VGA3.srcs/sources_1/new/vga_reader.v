d2b7424978a16b45ce962abd3759e08d47a5078a
shtuk41 <shtuk41@gmail.com>
2025-09-12 18:17:46 -0500

    saving changes.  using BRAM to share state between AXI and PL still not working.  But both work independently now including vga_reader driving colors

--- VGA3/VGA3.srcs/sources_1/new/vga_reader.v
+++ VGA3/VGA3.srcs/sources_1/new/vga_reader.v
@@ -21,28 +21,53 @@
 
 
 module vga_reader(
-    (* X_INTERFACE_PARAMETER = "FREQ_HZ 25022644, PHASE 0.0, CLK_DOMAIN design_1_clk" *)
-    (* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 clk CLK" *)
-    input wire clk,
-    //input wire [15:0] addr,
-    output reg[11:0] pixel,
-    //BRAM interface
-    output wire [31:0] bram_addr,
+    input wire clk,              // 50 MHz clock
+    output reg [11:0] pixel,    // VGA pixel
+    // BRAM interface
+    output reg [31:0] bram_addr,
     input wire [31:0] bram_dout,
     output wire bram_en,
-    output wire bram_rst            // add reset
-    );
-    
-    assign bram_addr = 32'h00000000;
-    assign bram_en   = 1'b1;
+    output wire bram_rst,
+    output reg [31:0] bram_dinb,
+    output reg [3:0] bram_web,
+    // LEDs
+    output reg [3:0] leds
+);
+
+    assign bram_en  = 1'b1;
     assign bram_rst = 1'b0;
-    
+
+    // Synchronized read
+    reg [31:0] bram_dout_sync;
+    always @(posedge clk) bram_dout_sync <= bram_dout;
+
+    // Color and timing
+    reg [31:0] color = 0;
+    reg [25:0] counter = 0;  // Enough for ~50M cycles (1 second at 50MHz)
+
     always @(posedge clk) begin
-        case(bram_dout)
-            32'h00000000: pixel <= 12'h00f;
-            32'h00000001: pixel <= 12'hf00;
-            32'h00000002: pixel <= 12'h0f0;
-            default: pixel <= 12'hff0;
+        bram_addr <= 0;      // always write to address 0
+
+        // Counter for ~1 second timing
+        if (counter == 50_000_000-1) begin
+            counter <= 0;
+            color <= (color < 2) ? color + 1 : 0;
+
+            // Write color to BRAM
+            bram_web <= 4'b1111;
+            bram_dinb <= color;
+        end else begin
+            counter <= counter + 1;
+            bram_web <= 4'b0000;
+        end
+
+        // Update pixel and LEDs based on BRAM content
+        case (bram_dout_sync)
+            32'h0:  begin pixel <= 12'h00f; leds <= 4'b1101; end
+            32'h1:  begin pixel <= 12'hf00; leds <= 4'b0010; end
+            32'h2:  begin pixel <= 12'h0f0; leds <= 4'b0100; end
+            default: begin pixel <= 12'hff0; leds <= 4'b1001; end
         endcase
     end
+
 endmodule
--- VGA3_workspace/VGA3/main.cpp
+++ VGA3_workspace/VGA3/main.cpp
@@ -15,15 +15,16 @@
 
 int main() {
 	XGpio_Config *cfg_ptr;
-	XGpio led_device, btn_device;
+	//XGpio led_device;
+	XGpio btn_device;
 	XBram Bram;	/* The Instance of the BRAM Driver */
 	u32 data;
 
 	xil_printf("Entered function main\r\n");
 
 	// Initialize LED Device
-	cfg_ptr = XGpio_LookupConfig(LED_ID);
-	XGpio_CfgInitialize(&led_device, cfg_ptr, cfg_ptr->BaseAddress);
+	//cfg_ptr = XGpio_LookupConfig(LED_ID);
+	//XGpio_CfgInitialize(&led_device, cfg_ptr, cfg_ptr->BaseAddress);
 
 	// Initialize Button Device
 	cfg_ptr = XGpio_LookupConfig(BTN_ID);
@@ -33,7 +34,7 @@ int main() {
 	XGpio_SetDataDirection(&btn_device, BTN_CHANNEL, BTN_MASK);
 
 	// Set Led Tristate
-	XGpio_SetDataDirection(&led_device, LED_CHANNEL, 0);
+	//XGpio_SetDataDirection(&led_device, LED_CHANNEL, 0);
 
 	XBram_Config *ConfigPtr;
 	ConfigPtr = XBram_LookupConfig(XPAR_BRAM_0_DEVICE_ID);
@@ -72,6 +73,8 @@ int main() {
 
 	int out_data;
 
+	int ii = 0;
+
 	/*int pixelsNum = 60;
 
 	for (int ii = 0; ii < pixelsNum; ii+=4)
@@ -87,31 +90,36 @@ int main() {
 		xil_printf("%d: %d\r\n", ii, out_data);
 	}*/
 
+	out_data = XBram_ReadReg(XPAR_BRAM_0_DEVICE_ID, 0);
+	xil_printf("%d \r\n", out_data);
+
+	u32 data_prev;
+
 	while (1)
 	{
-		XBram_WriteReg(XPAR_BRAM_0_DEVICE_ID, 0,0);
-		out_data = XBram_ReadReg(XPAR_BRAM_0_DEVICE_ID, 0);
-		xil_printf("%d \r\n", out_data);
-		sleep(5);
-
-		XBram_WriteReg(XPAR_BRAM_0_DEVICE_ID, 0,1);
-		out_data = XBram_ReadReg(XPAR_BRAM_0_DEVICE_ID, 0);
-		xil_printf("%d \r\n", out_data);
-		sleep(5);
-
-		XBram_WriteReg(XPAR_BRAM_0_DEVICE_ID, 0,2);
-		out_data = XBram_ReadReg(XPAR_BRAM_0_DEVICE_ID, 0);
-		xil_printf("%d \r\n", out_data);
-		sleep(5);
-
 
 		data = XGpio_DiscreteRead(&btn_device, BTN_CHANNEL);
 		data &= BTN_MASK;
-		if (data != 0) {
-			data = LED_MASK;
-		} else {
-			data = 0;
+
+		if (data == 0x1 && data != data_prev)
+		{
+			out_data = XBram_ReadReg(XPAR_BRAM_0_DEVICE_ID, 0);
+			xil_printf("%d \r\n", out_data);
 		}
-		XGpio_DiscreteWrite(&led_device, LED_CHANNEL, data);
+		else if (data == 0x2 && data != data_prev)
+		{
+			if (ii == 0 || ii == 1)
+			{
+				ii+=1;
+			}
+			else
+			{
+				ii=0;
+			}
+
+			XBram_WriteReg(XPAR_BRAM_0_DEVICE_ID, 0,ii);
+		}
+
+		data_prev = data;
 	}
 }
